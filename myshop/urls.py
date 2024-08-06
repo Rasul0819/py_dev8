06b398af6e10cab19822b3d5d78e536e6f9ae03c
Rasul0819 <rasulabdikerimov87@gmail.com>
2024-08-06 17:36:02 +0500

    detail

--- myshop/urls.py
+++ myshop/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.homepage,name='home')
+    path('',views.homepage,name='home'),
+    path('product/<int:id>',views.product_detail,name='detail'),
 ]
--- myshop/views.py
+++ myshop/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from . import models
 # Create your views here.
 def homepage(request):
     books = models.Books.objects.all()
     return render(request,'home.html',{'kitaplar':books})
+
+def product_detail(request,id):
+    book = get_object_or_404(models.Books,id=id)
+    return render(request,'detail.html',{'book':book})
+
